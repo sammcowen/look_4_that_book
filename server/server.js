f07cc55670de042c6b6bd6238b2ab2bde7de1b89
HBK <sammcowen@gmail.com>
2022-05-21 12:16:46 -0700

    server.js set up

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// import typeDefs and resolvers
+const {typeDefs, resolvers} = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,10 +27,16 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+}('*', (req,res) => {
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception:' + err);
 });
